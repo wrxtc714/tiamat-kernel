dfd1408478d4520edc1f338dbf5290bf19065020
cayniarb <charlton.danielk@gmail.com>
2011-10-08 15:46:07 -0400

    msm: acpuclock-8x60: adjust freq and l2 tables
    remove bus OC past 1512Mhz (per show-p1984)
    add some entries
    change some voltages

--- arch/arm/mach-msm/tiamat.h
+++ arch/arm/mach-msm/tiamat.h
@@ -14,15 +14,15 @@
 /* Frequency parameters */
 #define MAX_AXI			310500	/* KHz */
 #define SCPLL_LOW_VDD_FMAX	594000	/* KHz */
-#define FREQ_MAX		1836000	/* kHz */
+#define FREQ_MAX		2052000	/* kHz */
 
 /* Votage parameters */
-#define VOLTAGE_MIN		800000	/* uV */
-#define VOLTAGE_MAX		1350000	/* uV */
+#define VOLTAGE_MIN		700000	/* uV */
+#define VOLTAGE_MAX		1450000	/* uV */
 #define SCPLL_LOW_VDD		1000000 /* uV */
 #define SCPLL_NOMINAL_VDD	1100000 /* uV */
 
 /* Other parameters */
-#define FREQ_TBL_SIZE		30	/* number */
+#define FREQ_TBL_SIZE		35	/* number */
 
 #endif /* TIAMAT_H */
